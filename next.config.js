cab1b104c3ab0d1563af841107a7b7ec2426d2ac
Leorio Nguyen <thanhbinhbk888@gmail.com>
2023-09-19 21:24:22 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@
 const nextConfig = {
     reactStrictMode: true,
     output: 'export',
-    basePath: '/nextjs-github-pages',
+    basePath: "/nextjs-github-pages",
+    assetPrefix: 'https://binh168.github.io/phank_wedding_final/',
     images: {
         unoptimized: true,
-  }
+        domains: ['binh168.github.io/phank_wedding_final']
+    }
 }
 
 module.exports = nextConfig
