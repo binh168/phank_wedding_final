4696bf8403a79f0a4846e1e3f25cc8e8b4f6916d
Leorio Nguyen <thanhbinhbk888@gmail.com>
2023-09-21 22:32:22 +0700

    pages router

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // reactStrictMode: true,
-    output: 'export',
-    basePath: "/phank_wedding_final",
-    // assetPrefix: 'https://binh168.github.io/phank_wedding_final',
-    // images: {
-    //     unoptimized: true,
-    //     // loader: 'imgix',
-    //     // path: 'leorio-649772974.imgix.net',
-    // }
+  reactStrictMode: true,
+  output: 'export',
+  basePath: "/phank_wedding_final",
+  images: {
+      unoptimized: true,
+      // loader: 'imgix',
+      // path: 'leorio-649772974.imgix.net',
+  }
 }
 
 module.exports = nextConfig
