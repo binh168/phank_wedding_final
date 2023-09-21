2d83aaf8dfa622be7ac716ebc3410867f6f528e7
Leorio Nguyen <thanhbinhbk888@gmail.com>
2023-09-21 22:46:25 +0700

    app router

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'export',
-  basePath: "/phank_wedding_final",
-  images: {
-      unoptimized: true,
-      // loader: 'imgix',
-      // path: 'leorio-649772974.imgix.net',
-  }
+    // reactStrictMode: true,
+    output: 'export',
+    basePath: "/phank_wedding_final",
+    // assetsPrefix: "https://binh168.github.io/phank_wedding_final/",
+    images: {
+        unoptimized: true,
+    }
 }
 
 module.exports = nextConfig
